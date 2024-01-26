ef5b864b0e83a8c8d39eef7dc1cbf2a9e1fdc726
Jacky Chen <chenjacky32@gmail.com>
2024-01-26 11:51:22 +0700

    feat:Fitur tambah dan hapus to-do-list

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,74 @@ const ListItems = [
 
 function App() {
   return (
-    <div className="app">
-      <Logo></Logo>
-      <Form></Form>
-      <List></List>
-      <Status></Status>
-    </div>
+    <>
+      <div className="app">
+        <Logo></Logo>
+        <Form></Form>
+        <List></List>
+        <Status></Status>
+      </div>
+      <div className="todolist">
+        <Title></Title>
+        <InputText></InputText>
+      </div>
+    </>
+  );
+}
+
+function InputText() {
+  const [notes, setNotes] = useState([
+    {
+      id: 1,
+      text: "hello",
+    },
+  ]);
+  const [inputNote, setInputNote] = useState("");
+
+  function HandleInputChange(e) {
+    setInputNote(e.target.value);
+  }
+
+  function HandleSubmit(e) {
+    e.preventDefault();
+    if (inputNote.trim() !== "") {
+      const newNote = {
+        id: new Date().getTime(),
+        text: inputNote,
+      };
+      setNotes([...notes, newNote]);
+      setInputNote("");
+    }
+  }
+
+  function HandleDelete(id) {
+    const updateNotes = notes.filter((note) => note.id !== id);
+    setNotes(updateNotes);
+  }
+
+  return (
+    <>
+      <form className="note-input" onSubmit={HandleSubmit}>
+        <input type="text" value={inputNote} placeholder="addnote" onChange={HandleInputChange} />
+        <button>Add</button>
+      </form>
+      <ul className="note-list">
+        {notes.map((item) => (
+          <ul key={item.id}>
+            {item.text}
+            <button onClick={() => HandleDelete(item.id)}>Hapus</button>
+          </ul>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+function Title() {
+  return (
+    <>
+      <h1>To Do List &#9989;</h1>
+    </>
   );
 }
 
