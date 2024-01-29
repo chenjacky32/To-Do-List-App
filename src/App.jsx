3a7967dc1cd3f6232d998d3c470991d476d42380
Jacky Chen <chenjacky32@gmail.com>
2024-01-29 10:25:29 +0700

    feat:tambah fitur hapus Go-Check

--- src/App.jsx
+++ src/App.jsx
@@ -12,18 +12,114 @@ function App() {
     setListItem((listitem) => [...listitem, item]);
   }
 
+  function handledeleteitem(id) {
+    setListItem((listItem) => listItem.filter((item) => item.id !== id));
+  }
+
   return (
     <>
       <div className="app">
         <Logo></Logo>
         <Form onadditem={handleadditem}></Form>
-        <List items={listItem}></List>
+        <List items={listItem} onDeleteItems={handledeleteitem}></List>
         <Status></Status>
       </div>
       <div className="todolist">
         <Title></Title>
         <InputText></InputText>
       </div>
+    </>
+  );
+}
+
+//  Batas awal Section 1
+
+function Logo() {
+  return (
+    <>
+      <span className="logo"> &#128221; Go Check &#9989;</span>
+    </>
+  );
+}
+function Form({ onadditem }) {
+  const [title, setTitle] = useState("");
+
+  function Handlesubmit(e) {
+    e.preventDefault();
+
+    if (!title) return;
+
+    const newItem = {
+      id: Date.now(),
+      title,
+      done: false,
+    };
+
+    onadditem(newItem);
+    setTitle("");
+    // console.log(e);
+  }
+  return (
+    <>
+      <form className="add-form" onSubmit={Handlesubmit}>
+        <h3>Silahkan isi apa yg mau dicatat</h3>
+        <input
+          type="text"
+          name="title"
+          id=""
+          value={title}
+          onChange={(e) => {
+            console.log(e);
+            setTitle(e.target.value);
+          }}
+        />
+        <button>Add</button>
+      </form>
+    </>
+  );
+}
+function List({ items, onDeleteItems }) {
+  return (
+    <>
+      <div className="list">
+        <ul>
+          {items.map((item) => (
+            <Item key={item.id} item={item} onDeleteItems={onDeleteItems} />
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+}
+
+function Item({ item, onDeleteItems }) {
+  return (
+    <li>
+      <input type="checkbox" />
+      <span style={{ textDecoration: item.done ? "line-through" : "" }}>{item.title}</span>
+      <button onClick={() => onDeleteItems(item.id)}>❌</button>
+    </li>
+  );
+}
+
+function Status() {
+  return (
+    <>
+      <footer className="stats">
+        <span> &#128221; Kamu punya x catatan yg dichecklist &#9989;</span>
+      </footer>
+    </>
+  );
+}
+
+//Batas akhir section 1
+
+// Batas awal section 2
+
+function Title() {
+  return (
+    <>
+      <h1>To Do List &#9989;</h1>
     </>
   );
 }
@@ -76,90 +172,6 @@ function InputText() {
   );
 }
 
-function Title() {
-  return (
-    <>
-      <h1>To Do List &#9989;</h1>
-    </>
-  );
-}
-
-function Logo() {
-  return (
-    <>
-      <span className="logo"> &#128221; Go Check &#9989;</span>
-    </>
-  );
-}
-function Form({ onadditem }) {
-  const [title, setTitle] = useState("");
-
-  function Handlesubmit(e) {
-    e.preventDefault();
-
-    if (!title) return;
-
-    const newItem = {
-      id: Date.now(),
-      title,
-      done: false,
-    };
-
-    onadditem(newItem);
-    setTitle("");
-    // console.log(e);
-  }
-  return (
-    <>
-      <form className="add-form" onSubmit={Handlesubmit}>
-        <h3>Silahkan isi apa yg mau dicatat</h3>
-        <input
-          type="text"
-          name="title"
-          id=""
-          value={title}
-          onChange={(e) => {
-            console.log(e);
-            setTitle(e.target.value);
-          }}
-        />
-        <button>Add</button>
-      </form>
-    </>
-  );
-}
-function List({ items }) {
-  return (
-    <>
-      <div className="list">
-        <ul>
-          {items.map((item) => (
-            <Item key={item.id} item={item} />
-          ))}
-        </ul>
-      </div>
-    </>
-  );
-}
-
-function Item({ item }) {
-  return (
-    <li>
-      <input type="checkbox" />
-      <span style={{ textDecoration: item.done ? "line-through" : "" }}>{item.title}</span>
-      <button>❌</button>
-    </li>
-  );
-}
-
-function Status() {
-  return (
-    <>
-      <footer className="stats">
-        <span> &#128221; Kamu punya x catatan yg dichecklist &#9989;</span>
-      </footer>
-    </>
-  );
-}
+// Batas akhir section 2
 
 export default App;
